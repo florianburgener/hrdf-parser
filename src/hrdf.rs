f26c3a98665b7dc2c0f57446ebbc0986b5842320
Marc Gay-Balmaz <marc.gay-balmaz@unige.ch>
2026-06-16 16:21:49 +0200

    [WIP] Fix: add clone derivations for enum removabletype

--- src/hrdf.rs
+++ src/hrdf.rs
@@ -14,7 +14,7 @@ use sha2::{Digest, Sha256};
 use url::Url;
 use zip::ZipArchive;
 
-#[derive(Hash, Eq, PartialEq, Serialize, Deserialize)]
+#[derive(Hash, Eq, PartialEq, Serialize, Deserialize, Clone)]
 pub enum RemovableTypes {
     Line,
     Stop,
